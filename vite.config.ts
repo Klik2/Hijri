d447784e2b2f60a52f0ad251dffc7745e1b4892e
Klik2 <rayandausbr@gmail.com>
2025-12-27 13:32:20 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/Hijri/", //
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
